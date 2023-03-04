5b831ddf8e514252ba43e2c4c6ae0462bbc6c3d7
mayank.tomar <mayank.tomar@u-next.com>
2023-03-04 21:29:03 +0530

    Yogilesh changes

--- src/Display.js
+++ src/Display.js
@@ -9,6 +9,7 @@ export default function Display(props) {
     console.log("Change in Display.js");
 
     console.log("Branch name change in Display");
+    console.log("Yogilesh changes");
   }, [props.getNumbers])
   
 
